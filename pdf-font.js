d6ee4ddb2fd8f43f19a85dea10853d34323fd0c0
tsutomunakayama0616-oss <tsutomu.nakayama0616@gmail.com>
2026-02-03 02:13:12 +0900

    Update pdf-font.js

--- pdf-font.js
+++ pdf-font.js
@@ -1,7 +1,19 @@
-// pdf-font.js
+// 日本語フォントを jsPDF に登録する
 async function loadJapaneseFont(doc) {
-  const fontUrl = "./NotoSansJP-Regular.ttf";  // ← ここが重要
-  const fontData = await fetch(fontUrl).then(res => res.arrayBuffer());
-  doc.addFileToVFS("NotoSansJP.ttf", fontData);
-  doc.addFont("NotoSansJP.ttf", "NotoSansJP", "normal");
+  const fontUrl = "./NotoSansJP-Regular.ttf";
+
+  const fontData = await fetch(fontUrl)
+    .then(res => res.arrayBuffer())
+    .catch(err => {
+      console.error("フォント読み込み失敗:", err);
+    });
+
+  if (!fontData) {
+    alert("日本語フォントを読み込めませんでした。PDFが文字化けします。");
+    return;
+  }
+
+  // VFS にフォントを登録
+  doc.addFileToVFS("NotoSansJP-Regular.ttf", fontData);
+  doc.addFont("NotoSansJP-Regular.ttf", "NotoSansJP", "normal");
 }
