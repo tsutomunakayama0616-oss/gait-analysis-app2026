7393abf9854f9f773102e6fa06a58464b69c757d
tsutomunakayama0616-oss <tsutomu.nakayama0616@gmail.com>
2026-02-02 17:21:56 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "gait-app-cache-v1";
+const CACHE_NAME = "gait-app-cache-v3";  // ★バージョンを上げる
 const URLS_TO_CACHE = [
   "./",
   "./index.html",
@@ -8,7 +8,9 @@ const URLS_TO_CACHE = [
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(URLS_TO_CACHE);
+    })
   );
 });
 
@@ -16,9 +18,11 @@ self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((keys) =>
       Promise.all(
-        keys
-          .filter((key) => key !== CACHE_NAME)
-          .map((key) => caches.delete(key))
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
       )
     )
   );
